0fc28ea0ee52d8812053599917c6e8001279ce54
roselynda <roszelinda@hotmail.com>
2016-08-17 00:06:05 +0800

    unit
    unit type

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -2,7 +2,7 @@ class Parameter < ActiveRecord::Base
   belongs_to :component
 
   def unit_type_enum
-    ['Hz', 'dBm', 'MHz', 'nm', 'kW', 'MW', 'Joule', 'meter', 'km', 'THz', 'deg', 'dB/km']
+    ['Hz', 'dBm', 'MHz', 'nm', 'kW', 'MW', 'Joule', 'meter', 'km', 'THz', 'deg', 'dB/km','ms']
   end
 
   def value_type_enum
